9746fc1adc8c7498eaa59dce525ef6e94c0dd27c
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-12 15:46:13 +0200

    Edit: added get product controller and one-product-page.html

--- src/main/java/lv/venta/controller/MyFirstController.java
+++ src/main/java/lv/venta/controller/MyFirstController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lv.venta.model.Product;
+
 @Controller
 public class MyFirstController {
 	
@@ -26,5 +28,17 @@ public class MyFirstController {
 		model.addAttribute("box", data);
 		return "data-page";//it will show a data-page.html with data "Karina: 6"
 	}
+	
+	@GetMapping("/testproduct")//localhost:8080/testproduct
+	public String getControllerSendProduct(Model model) {
+		Product testProduct = new Product("Banana", 1.99f, "Eco, yellow", 5);
+		model.addAttribute("box", testProduct);
+		return "one-product-page";//it will show an one-product-page.html with test product (Banana, 1.99 eur ...)
+		
+	}
+	//create new product
+	//add this product in model
+	//specify which html file should be showed
+	//create the html file
 
 }

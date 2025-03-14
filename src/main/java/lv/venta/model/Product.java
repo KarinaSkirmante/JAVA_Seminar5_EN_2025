b127086c5df683b0b6c4f54c87ed12ab8b651bb4
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-14 13:28:27 +0200

    Edit: added validations in Product model class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,10 @@
 package lv.venta.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,9 +18,26 @@ import lombok.ToString;
 public class Product {
 	//1. variables
 	private long id; //TODO auto increment from DB
+	
+	@NotNull
+	@Pattern(regexp = "[A-Z]{1}[a-z ]{2,20}")
+	//@Size(min = 3, max = 21) <- I can use it if there is no min or max inside the regex
 	private String title;
+	
+	
+	
+	@Min(0)
+	@Max(1000)
 	private float price;
+	
+	@NotNull
+	@Pattern(regexp = "[A-Za-z0-9 ,.;:]+")
+	@Size(min = 5, max = 100)
 	private String description;
+	
+	
+	@Min(0)
+	@Max(100)
 	private int quantity;
 	//2. getters - lombok
 	//3. setters - lombok

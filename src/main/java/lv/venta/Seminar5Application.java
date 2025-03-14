ed27b6f78b4d4131d3537f0d59bee1e4fc6d5e5e
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-14 14:31:55 +0200

    Add: added repo interface for product and command line runner function for testing (not completed)

--- src/main/java/lv/venta/Seminar5Application.java
+++ src/main/java/lv/venta/Seminar5Application.java
@@ -1,13 +1,30 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class Seminar5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Seminar5Application.class, args);
+	}
+	
+	//TODO need to be run automatically
+	public CommandLineRunner testModel(IProductRepo prodRepo) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				Product p1 = new Product("Banana", 1.99f, "Eco, yellow", 5);
+				prodRepo.save(p1);
+				
+			}
+		};
 	}
 
 }

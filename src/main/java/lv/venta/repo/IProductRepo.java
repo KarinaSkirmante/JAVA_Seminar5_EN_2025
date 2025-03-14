994d79355c317cc6e730b4dc5bcbcfe731f551be
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-14 15:52:31 +0200

    Add: added Service class for CRUD product - retrieve functionality

--- src/main/java/lv/venta/repo/IProductRepo.java
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -1,8 +1,10 @@
 package lv.venta.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import lv.venta.model.Product;
+
 
 public interface IProductRepo extends CrudRepository<Product, Long> {
 

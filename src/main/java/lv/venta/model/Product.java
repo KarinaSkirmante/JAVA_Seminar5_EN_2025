8a6c3c5e79abb1d129483f0a2f6802f8c55d0f79
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-14 13:59:00 +0200

    Edit: added Data JPA library and annotations in Product model clas

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,10 +1,17 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,29 +22,41 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 
+@Table(name = "ProductTable") //mysql - product_table, H2 - PRODUCT_TABLE
+@Entity
 public class Product {
 	//1. variables
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)//<-this means that setter for id will be not visible/usable
 	private long id; //TODO auto increment from DB
 	
+	
+
 	@NotNull
 	@Pattern(regexp = "[A-Z]{1}[a-z ]{2,20}")
 	//@Size(min = 3, max = 21) <- I can use it if there is no min or max inside the regex
+	@Column(name = "Title")
 	private String title;
 	
 	
 	
 	@Min(0)
 	@Max(1000)
+	@Column(name = "Price")
 	private float price;
 	
 	@NotNull
 	@Pattern(regexp = "[A-Za-z0-9 ,.;:]+")
 	@Size(min = 5, max = 100)
+	@Column(name = "Description")
 	private String description;
 	
 	
 	@Min(0)
 	@Max(100)
+	@Column(name = "Quantity")
 	private int quantity;
 	//2. getters - lombok
 	//3. setters - lombok

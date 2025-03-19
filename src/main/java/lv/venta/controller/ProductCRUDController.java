eceb0f8590f4c826e242b4e3c72f71ebabd2592c
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-19 14:32:00 +0200

    Add: create controller for Product - retrieve by id (with ?id=10)

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.model.Product;
 import lv.venta.service.IProductCRUDService;
@@ -36,6 +37,22 @@ public class ProductCRUDController {
 		}
 		}
 	//R - retrieve by id
+	@GetMapping("/one")//localhost:8080/product/crud/one?id=3
+	public String getControllerOneProductById(@RequestParam(name = "id") long id, Model model)
+	{
+		try
+		{
+			Product oneProduct = prodService.retreiveById(id);
+			model.addAttribute("box", oneProduct);//will add only one product in box
+			return "show-one-product-page";//this will show show-one-product-page.html with found product
+		}
+		catch (Exception e) {
+			model.addAttribute("box", e.getMessage());
+			return "error-page";//this will show error-page.html with Exception message
+
+		}
+	}
+	
 	//U - update by id
 	//D - delete by id
 	

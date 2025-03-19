86b4860225c7312a7df53becf06e3c81cdbae36a
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-19 14:49:13 +0200

    Edit: create controller for Product - retrieve by id (with /10)

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -36,7 +37,7 @@ public class ProductCRUDController {
 			return "error-page";//this will show error-page.html with Exception message
 		}
 		}
-	//R - retrieve by id
+	//R - retrieve by id (the first approach)
 	@GetMapping("/one")//localhost:8080/product/crud/one?id=3
 	public String getControllerOneProductById(@RequestParam(name = "id") long id, Model model)
 	{
@@ -52,6 +53,24 @@ public class ProductCRUDController {
 
 		}
 	}
+	
+	//R - retrieve by id (the second approach)
+	@GetMapping("/all/{id}")//localhost:8080/product/crud/all/3
+	public String getControllerOneProductById2(@PathVariable(name = "id") long id, Model model)
+	{
+		try
+		{
+			Product oneProduct = prodService.retreiveById(id);
+			model.addAttribute("box", oneProduct);//will add only one product in box
+			return "show-one-product-page";//this will show show-one-product-page.html with found product
+		}
+		catch (Exception e) {
+			model.addAttribute("box", e.getMessage());
+			return "error-page";//this will show error-page.html with Exception message
+
+		}
+	}
+	
 	
 	//U - update by id
 	//D - delete by id
